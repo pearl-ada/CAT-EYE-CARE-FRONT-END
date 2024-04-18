0bc3732a40ffca32b2a87d878305aaaa3b52f702
pearl-ada <adddaeeze@gmail.com>
2024-04-18 11:19:50 +0100

    a few php

---
+++ DATABASE/register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/register.css">
+    <title>RESGISTER</title>
+</head>
+<body>
+<div class="headers">
+        <h1>CAT-EYE CARE</h1>
+        <h3>WELCOME</h3>
+     </div>
+     <form action="" class="form">
+        <h1>SIGN IN</h1>
+    <div class="form-container">
+
+<form action="" method="post" enctype="multipart/form-data">
+    <input type="text" name="name" placeholder="enter username" class="box" required>
+        <input type="email" name="email" class="box" placeholder="Enter Email" required>
+        <input type="password" name="Password" class="box" placeholder="Enter Password" required>
+        <input type="password" name="cpassword" placeholder="confirm password" class="box" requred>
+        <input type="submit" value="SIGN IN" id="submit">
+        <p>
+    <a href="../HTML/login.html">Have an account? Login here</a></p>
+</form>
+</form>
+    </div>
+</body>
+</html>
