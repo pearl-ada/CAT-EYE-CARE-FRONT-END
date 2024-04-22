25388682791703bdf57e397af4a3612c6375d20a
pearl-ada <adddaeeze@gmail.com>
2024-04-22 17:17:50 +0100

    php changes

---
+++ DATABASE/login.php
@@ -0,0 +1,81 @@
+<?php
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass =  ($_POST['password']);
+    $cpass = ($_POST['cpassword']);
+    $user_type = $_POST['user_type'];
+
+   $select = " SELECT * FROM `user_form` WHERE email = '$email' && 
+    password = '$pass' ";
+
+    $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0)
+   {
+    $row = mysqli_fetch_array($result);
+
+    if($row ['user_type'] == 'user'){
+        $_SESSION['user_name'] = $row['name'];
+        
+   
+        header("Location: ../HTML/dashboard.html");
+    }
+    }else{
+            $error[] = 'incorrect email or password!';
+        }
+    }
+    
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN PAGE</title>
+    <link rel="stylesheet" href="../CSS/login.css">
+</head>
+<body>
+    <div class="headers">
+    <h1>CAT-EYE CARE</h1>
+    <h3>WELCOME</h3>
+ </div>
+ <form action="login.php" method="post" enctype="multipart/form-data">
+    <div class="form">
+        <h1>LOGIN</h1>
+       <?php
+    if(isset($error)){
+        foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+       }
+   }
+    ?>
+
+        <input type="email" name="email" class="box" placeholder="Enter Username" required>
+        <input type="password" name="Password" class="box" placeholder="Enter Password" required>
+        <input type="submit" value="LOGIN" id="submit"> 
+      <p>  <a href="../HTML/signup.html">Don't have an account? Sign in here</a></p>
+    </div>
+</form>
+</body>
+</html>
--- DATABASE/register.php
+++ DATABASE/register.php
@@ -1,3 +1,41 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $pass = mysqli_real_escape_string($conn, ($_POST['password']));
+    $cpass = mysqli_real_escape_string($conn, ($_POST['cpassword']));
+
+    $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND
+    password = '$pass'") or die('query failed');
+
+    if(mysqli_num_rows($select) > 0){
+        $message[] = 'user already exist';
+    }else{
+        if($pass != $cpass){
+            $message[] = 'confirm password not matched!';
+            
+        }else{
+            $insert = mysqli_query($conn, "INSERT INTO `user_form` (name, email, password) VALUES 
+            ('$name', '$email', '$pass')") or die('query failed');
+
+            header("Location: ../HTML/dashboard.html");
+
+            
+        }
+    }
+
+
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,19 +49,25 @@
         <h1>CAT-EYE CARE</h1>
         <h3>WELCOME</h3>
      </div>
-     <form action="" class="form">
+     <form method="post" action="register.php" class="form-container">
         <h1>SIGN IN</h1>
-    <div class="form-container">
+    <?php
+    if(isset($message)){
+        foreach($message as $message){
+            echo '<div class="message">'.$message.'</div>';
+        }
+    }
+    ?>
 
 <form action="" method="post" enctype="multipart/form-data">
     <input type="text" name="name" placeholder="enter username" class="box" required>
         <input type="email" name="email" class="box" placeholder="Enter Email" required>
-        <input type="password" name="Password" class="box" placeholder="Enter Password" required>
+        <input type="password" name="password" class="box" placeholder="Enter Password" required>
         <input type="password" name="cpassword" placeholder="confirm password" class="box" requred>
-        <input type="submit" value="SIGN IN" id="submit">
+       <input type="submit" name="submit" value="SIGN IN" id="submit">
         <p>
     <a href="../HTML/login.html">Have an account? Login here</a></p>
-</form>
+</form> 
 </form>
     </div>
 </body>
